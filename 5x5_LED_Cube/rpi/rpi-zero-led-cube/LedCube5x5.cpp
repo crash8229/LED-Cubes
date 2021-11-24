b98e1f99a5a0fae32bff45ee35f126530d6c9c67
Mike Umana <mu304007@gmail.com>
2021-11-23 21:20:38 -0800

    Renamed folder Added new class and waveform workspace

---
+++ 5x5_LED_Cube/rpi/rpi-zero-led-cube/LedCube5x5.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Umana on 11/23/2021.
+//
+
+#include "LedCube5x5.h"
---
+++ 5x5_LED_Cube/rpi/rpi-zero-led-cube/LedCube5x5.h
@@ -0,0 +1,14 @@
+//
+// Created by Mike Umana on 11/23/2021.
+//
+
+#ifndef RPI_LED_CUBE_LEDCUBE5X5_H
+#define RPI_LED_CUBE_LEDCUBE5X5_H
+
+
+class LedCube5x5 {
+
+};
+
+
+#endif //RPI_LED_CUBE_LEDCUBE5X5_H
