8c6df902484b9e84d7f84fdb84753c89eaa2949c
crash8229 <mu304007@gmail.com>
2022-03-22 23:46:58 -0700

    SD card configured correctly

---
+++ 5x5_LED_Cube/rpi/rpi_pico_driver/config.h
@@ -0,0 +1,32 @@
+//
+// Created by mike on 3/21/22.
+//
+
+#ifndef RPI_PICO_DRIVER_CONFIG_H
+#define RPI_PICO_DRIVER_CONFIG_H
+
+// TODO: Figure out how to turn off led when interacting to SD
+#define DEBUG 1
+//#define NO_PICO_LED
+//#define USE_LED 0
+
+// TLC5940 Defines
+#define TLC_PORT spi0
+#define TLC_MISO 16
+#define TLC_CS   17
+#define TLC_SCLK  18
+#define TLC_MOSI 19
+#define TLC_NUM 2  // Number of ICs chained together
+#define TLC_XLAT 13
+#define TLC_BLANK 14
+#define TLC_GSCLK 15
+
+// SD Defines
+#define SD_PORT spi1
+#define SD_MISO 8
+#define SD_CS   9
+#define SD_SCK  10
+#define SD_MOSI 11
+#define SD_RATE 1000 * 1000  //1MHz
+
+#endif //RPI_PICO_DRIVER_CONFIG_H
--- 5x5_LED_Cube/rpi/rpi_pico_driver/main.cpp
+++ 5x5_LED_Cube/rpi/rpi_pico_driver/main.cpp
@@ -8,37 +8,14 @@
 #include <iomanip>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
-#include "hardware/i2c.h"
+
 #include "tlc5940.h"
-#include "w25q64.h"
+#include "config.h"
+#include "sd_card.h"
+#include "ff.h"
 
-#define DEBUG 1
 
-// TLC5940 Defines
-#define TLC_PORT spi0
-#define TLC_MISO 16
-#define TLC_CS   17
-#define TLC_SCK  18
-#define TLC_MOSI 19
-#define TLC_NUM 2  // Number of ICs chained together
-#define TLC_XLAT 13
-#define TLC_BLANK 14
-#define TLV_GSCLK 15
-
-// W25Q64 Defines
-#define W25_PORT spi1
-#define W25_MISO 8
-#define W25_CS   9
-#define W25_SCK  10
-#define W25_MOSI 11
-
-// I2C Defines
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-//#define I2C_PORT i2c0
-//#define I2C_SDA 8
-//#define I2C_SCL 9
-
-const uint LED_PIN = 25;
+const uint LED = 25;
 
 std::string hexStr(uint8_t *data, uint32_t len)
 {
@@ -60,27 +37,10 @@ int main() {
     stdio_init_all();
     printf("\n");
 
-    const SPIConfig w25_config = {
-            W25_PORT,
-            W25_MISO,
-            W25_CS,
-            W25_SCK,
-            W25_MOSI
-    };
-    W25Q64 w25 = W25Q64(&w25_config);
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
 
-    const TLC5940Config tlc_config = {
-            TLC_PORT,
-            TLC_MISO,
-            TLC_CS,
-            TLC_SCK,
-            TLC_MOSI,
-            TLC_NUM,
-            TLC_XLAT,
-            TLC_BLANK,
-            TLV_GSCLK
-    };
-    TLC5940 tlc = TLC5940(&tlc_config);
+    TLC5940 tlc = TLC5940(TLC_PORT, TLC_MISO, TLC_CS, TLC_SCLK, TLC_MOSI, TLC_XLAT, TLC_BLANK, TLC_GSCLK, TLC_NUM);
 
     // Double Pyramid
     uint8_t data[5][32] = {{1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
@@ -88,82 +48,116 @@ int main() {
                            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                            {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}};
-
-    // I2C Initialisation. Using it at 400Khz.
-//    i2c_init(I2C_PORT, 400*1000);
-
-//    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-//    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-//    gpio_pull_up(I2C_SDA);
-//    gpio_pull_up(I2C_SCL);
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-//    while (1) {
-//        for (uint8_t *datum: data) {
-//            gpio_put(LED_PIN, 0);
-//            sleep_ms(250);
-//            gpio_put(LED_PIN, 1);
-//            tlc.setGrayscale(datum);
-//            sleep_ms(250);
-//        }
-//    }
+    printf("Drive TLC\n");
+    for (int i = 0; i < 3;i++) {
+        for (uint8_t *datum: data) {
+            gpio_put(LED, 0);
+            sleep_ms(100);
+            gpio_put(LED, 1);
+            tlc.setGrayscale(datum);
+            sleep_ms(100);
+        }
+    }
+    tlc.ledAllOff();
+    gpio_put(LED, 0);
 
     uint8_t data_buffer[1024];
     uint32_t bytes_read;
     uint8_t data_out[4] = {0xAA, 0xBB, 0xCC, 0xDD};
 
-    // Trigger pulse
-    gpio_init(2);
-    gpio_set_dir(2, GPIO_OUT);
-    gpio_put(2, 1);
-    gpio_put(2, 0);
+    // SD Card Testing
+    FRESULT fr;
+    FATFS fs;
+    FIL fil;
+    int ret;
+    char buf[100];
+    char filename[] = "test02.txt";
+    const TCHAR sd_drive[] = "0:";
 
-    //Bytes before
-//    bytes_read = w25.readData(0, 4, data_buffer);
-//    printf("Bytes read: %s\n", hexStr(data_out, bytes_read).c_str());
-
-    // Write enable
-//    printf("Reg1: %d\n", w25.getRawStatusRegister1());
-//    w25.writeEnable();
-//    printf("Reg1: %d\n", w25.getRawStatusRegister1());
-
-    // Write data
-//    w25.writeEnable();
-//    printf("Reg1: %d\n", w25.getRawStatusRegister1());
-//    absolute_time_t start = get_absolute_time();
-//    w25.writeDataBlocking(0, 4, data_out);
-//    printf("Reg1: %d\n", w25.getRawStatusRegister1());
-//    printf("Time to write: %llu us\n", absolute_time_diff_us(start, get_absolute_time()));
-
-    // Read data
-//    start = get_absolute_time();
-//    bytes_read = w25.readData(0, 4, data_buffer);
-//    printf("\nTime to read: %llu us\n", absolute_time_diff_us(start, get_absolute_time()));
-//    printf("Bytes read: %s\n", hexStr(data_buffer, bytes_read).c_str());
-
-    // Erase 4KB
-//    start = get_absolute_time();
-//    w25.erase4KBlocking(0);
-//    printf("\nTime to erase 4KB: %llu us\n", absolute_time_diff_us(start, get_absolute_time()));
-//    bytes_read = w25.readData(0, 4, data_buffer);
-//    printf("Bytes read: %s\n", hexStr(data_buffer, bytes_read).c_str());
-
-    printf("Reg1: %d\n", w25.getRawStatusRegister1());
-    while (1) {
-        gpio_put(2, 1);
-        gpio_put(2, 0);
-        w25.writeEnable();
-        printf("Reg1: %d\n", w25.getRawStatusRegister1());
-        sleep_us(50);
-        w25.writeDisable();
-        printf("Reg1: %d\n", w25.getRawStatusRegister1());
-        sleep_us(50);
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
+    // Wait for user to press 'enter' to continue
+    printf("\r\nSD card test. Press 'enter' to start.\r\n");
+    while (true) {
+        buf[0] = getchar();
+        if ((buf[0] == '\r') || (buf[0] == '\n')) {
+            break;
+        }
     }
+
+    // Initialize SD card
+    if (!sd_init_driver()) {
+        printf("ERROR: Could not initialize SD card\r\n");
+        while (true);
+    }
+
+    // Mount drive
+    fr = f_mount(&fs, sd_drive, 1);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Open file for writing ()
+    fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not open file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Write something to file
+    ret = f_printf(&fil, "This is another test\r\n");
+    if (ret < 0) {
+        printf("ERROR: Could not write to file (%d)\r\n", ret);
+        f_close(&fil);
+        while (true);
+    }
+    ret = f_printf(&fil, "of writing to an SD card.\r\n");
+    if (ret < 0) {
+        printf("ERROR: Could not write to file (%d)\r\n", ret);
+        f_close(&fil);
+        while (true);
+    }
+
+    // Close file
+    fr = f_close(&fil);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not close file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Open file for reading
+    fr = f_open(&fil, filename, FA_READ);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not open file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Print every line in file over serial
+    printf("Reading from file '%s':\r\n", filename);
+    printf("---\r\n");
+    while (f_gets(buf, sizeof(buf), &fil)) {
+        printf(buf);
+    }
+    printf("\r\n---\r\n");
+
+    // Close file
+    printf("Closing file: '%s'\r\n", filename);
+    fr = f_close(&fil);
+    if (fr != FR_OK) {
+        printf("ERROR: Could not close file (%d)\r\n", fr);
+        while (true);
+    }
+
+    // Unmount drive
+    printf("Unmounting SD\r\n");
+    f_unmount(sd_drive);
+
+
+    // End infinite loop
+    printf("Program End\n");
+    while (true) {
+        sleep_ms(1000);
+    }
+
 }
 
 #pragma clang diagnostic pop
